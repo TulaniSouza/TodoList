b5da3c455ba22693ef6817ac1d860acbbfe61e04
Tulani <tulanidepaula@gmail.com>
2024-07-02 01:11:39 -0300

    todolist

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Home from "./Home/Home"
+import { GlobalStyle } from "./Home/Style";
+
+
+
+function App(){
+  return(
+    <>
+    <Home/>
+    <GlobalStyle/>
+    </>
+    
+  )
+}
+
+export default App
